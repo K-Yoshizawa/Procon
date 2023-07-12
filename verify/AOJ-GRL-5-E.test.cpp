b7560b96ead32641d593ff72d56e4d342b6efd48
log_K <naniyoshi.z@gmail.com>
2023-07-12 09:56:01 +0900

    Ver 1.8.3

--- verify/AOJ-GRL-5-E.test.cpp
+++ verify/AOJ-GRL-5-E.test.cpp
@@ -1,4 +1,5 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_E"
+#define IGNORE
 
 #include "../library/DataStructure/BinaryIndexedTreeDouble.hpp"
 #include "../library/Tree/HeavyLightDecomposition.hpp"
