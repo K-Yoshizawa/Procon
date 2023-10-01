a3e55fac5ac230e65fcb8cb8dd972decf4e878c8
KStepping <ksteppingstone36@gmail.com>
2023-10-02 01:05:54 +0900

    セグ木　書きました（ドキュメントは待ってくれ！！！！）

--- latest/DataStructure/SegmentTree.hpp
+++ latest/DataStructure/SegmentTree.hpp
@@ -1,5 +1,10 @@
-
-
+/**
+ * @file SegmentTree.hpp
+ * @author log K (lX57)
+ * @brief Segment Tree - セグメント木
+ * @version 2.0
+ * @date 2023-10-02
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -32,6 +37,13 @@ struct SegmentTree{
     }
 
     public:
+    /**
+     * @brief セグメント木を要素数 `Size` で初期化する。
+     * @param Size セグメント木の要素数
+     * @param Merge 区間取得を行う演算
+     * @param Monoid_Identity モノイドの単位元
+     * @param ZeroIndex 0-indexとして扱いたいか (default = `false`)
+     */
     SegmentTree(int Size, F Merge, const Monoid &Monoid_Identity, bool ZeroIndex = false) : f(Merge), __M1(Monoid_Identity), __ZeroIndex(ZeroIndex){
         __Size = 1;
         while(__Size < Size) __Size <<= 1;
@@ -39,17 +51,33 @@ struct SegmentTree{
         __Data.resize(2 * __Size, __M1);
     }
 
+    /**
+     * @brief セグメント木を構築する。
+     * @attention 必ず `set()` で初期値を代入してから呼び出すこと！
+     */
     void build(){
         for(int i = __Offset; i >= 1; --i){
             __Data[i] = f(__Data[i * 2 + 0], __Data[i * 2 + 1]);
         }
     }
 
+    /**
+     * @brief セグメント木の初期値を代入する。
+     * @param Index 代入先の要素番号 (default = 1-index)
+     * @param Value 代入する値
+     */
     void set(int Index, Monoid Value){
         __Check(Index + __ZeroIndex);
         __Data[__Offset + Index + __ZeroIndex] = Value;
     }
 
+    /**
+     * @brief セグメント木を配列 `Init_Data` で初期化する。
+     * @param Init_Data 初期データの配列
+     * @param Merge 区間取得を行う演算
+     * @param Monoid_Identity モノイドの単位元
+     * @param ZeroIndex 0-indexとして扱いたいか (default = `false`)
+     */
     SegmentTree(vector<Monoid> &Init_Data, F Merge, const Monoid &Monoid_Identity, bool ZeroIndex = false) : f(Merge), __M1(Monoid_Identity), __ZeroIndex(ZeroIndex){
         __Size = 1;
         while(__Size < (int)Init_Data.size()) __Size <<= 1;
@@ -61,6 +89,11 @@ struct SegmentTree{
         build();
     }
 
+    /**
+     * @brief 一点更新クエリを処理する。
+     * @param Index 更新先の要素番号 (default = 1-index)
+     * @param Value 更新する値
+     */
     void update(int Index, Monoid Value){
         __Check(Index + __ZeroIndex);
         int k = __Offset + Index + __ZeroIndex;
@@ -70,12 +103,23 @@ struct SegmentTree{
         }
     }
 
+    /**
+     * @brief 半開区間 `[Left, Right)` に対して区間取得クエリを行う。
+     * @param Left 半開区間の左端
+     * @param Right 半開区間の右端
+     * @return Monoid 取得した結果
+     */
     Monoid query(int Left, int Right){
         __Check(Left + __ZeroIndex);
         __Check(Right + __ZeroIndex - 1);
         return __query(Left + __ZeroIndex, Right + __ZeroIndex, 1, __Size + 1, 1);
     }
 
+    /**
+     * @brief 要素番号 `k` の要素を取得する。
+     * @param k 取得先の要素番号 (default = 1-index)
+     * @return Monoid 取得した結果
+     */
     Monoid get(int k){
         __Check(k + __ZeroIndex);
         return __Data[__Offset + k + __ZeroIndex];
