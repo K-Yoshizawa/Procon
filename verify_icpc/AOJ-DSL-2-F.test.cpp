5dcb9778f2573fe4f6d26a34f869404039f703da
log_K <naniyoshi.z@gmail.com>
2023-11-23 01:45:06 +0900

    icpc resub

--- verify_icpc/AOJ-DSL-2-F.test.cpp
+++ verify_icpc/AOJ-DSL-2-F.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_E"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_F"
 
 #include "../icpc/LazySegmentTree.hpp"
 
