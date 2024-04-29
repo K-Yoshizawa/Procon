3efda849e38383cc47c9884e5fa700df8d58fb41
K-Yoshizwa <naniyoshi.z@gmail.com>
2024-04-29 23:28:35 +0900

    Convex Hull Trick (Dynamic) を実装

--- library/DataStructure/ConvexHullTrick.hpp
+++ library/DataStructure/ConvexHullTrick.hpp
@@ -8,101 +8,112 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * @brief 直線 `ax + b` の集合に対して、`x` における最小値(最大値)を求める
+ * @tparam Line 直線の傾き `a` と切片 `b` の型
+ * @tparam Point 座標 `x` の型
+ */
 template<typename Line, typename Point = Line>
 struct ConvexHullTrick{
     private:
-    union{
-        map<Line, Line, greater<Line>> slp_inc;
-    };
-    map<Line, Point> slp_rht;
-    map<Point, Line> rht_slp;
+    map<Line, Line, greater<Line>> slope_to_intercept_;
+    map<Line, Point> slope_to_rightend_;
+    map<Point, Line> rightend_to_slope_;
     
-    bool is_max{false}, add_monotone{false}, query_monotone{false};
-    Point INF{numeric_limits<Point>::max() / 4};
+    bool is_max_{false};
+    Point inf_{numeric_limits<Point>::max() >> 2};
 
-    Point f(Line al, Line bl, Line ar, Line br){
+    Point calc_(Line al, Line bl, Line ar, Line br){
         return (Point)(br - bl) / (Point)(al - ar);
     }
 
-    void m_add(Line a, Line b){
-        if(slp_inc.contains(a) and slp_inc[a] <= b) return;
-        slp_inc[a] = b;
-        auto itr = slp_inc.find(a);
-        if(itr != slp_inc.begin() and next(itr) != slp_inc.end()){
+    void add_(Line a, Line b){
+        if(slope_to_intercept_.contains(a) and slope_to_intercept_[a] <= b) return;
+        slope_to_intercept_[a] = b;
+        auto itr = slope_to_intercept_.find(a);
+        if(itr != slope_to_intercept_.begin() and next(itr) != slope_to_intercept_.end()){
             auto [al, bl] = *prev(itr);
             auto [ar, br] = *next(itr);
-            if(f(al, bl, a, b) >= f(a, b, ar, br)){
-                slp_inc.erase(a);
-                if(slp_rht.contains(a)){
-                    rht_slp.erase(slp_rht[a]);
-                    slp_rht.erase(a);
+            if(calc_(al, bl, a, b) >= calc_(a, b, ar, br)){
+                slope_to_intercept_.erase(a);
+                if(slope_to_rightend_.contains(a)){
+                    rightend_to_slope_.erase(slope_to_rightend_[a]);
+                    slope_to_rightend_.erase(a);
                 }
                 return;
             }
         }
         auto litr = prev(itr);
-        while(itr != slp_inc.begin() and litr != slp_inc.begin()){
+        while(itr != slope_to_intercept_.begin() and litr != slope_to_intercept_.begin()){
             auto [all, bll] = *prev(litr);
             auto [al, bl] = *litr;
-            if(f(all, bll, al, bl) >= f(al, bl, a, b)){
-                slp_inc.erase(litr);
-                rht_slp.erase(slp_rht[al]);
-                slp_rht.erase(al);
+            if(calc_(all, bll, al, bl) >= calc_(al, bl, a, b)){
+                slope_to_intercept_.erase(litr);
+                rightend_to_slope_.erase(slope_to_rightend_[al]);
+                slope_to_rightend_.erase(al);
                 litr = prev(itr);
             }
             else break;
         }
         auto ritr = next(itr);
-        while(ritr != slp_inc.end() and next(ritr) != slp_inc.end()){
+        while(ritr != slope_to_intercept_.end() and next(ritr) != slope_to_intercept_.end()){
             auto [arr, brr] = *next(ritr);
             auto [ar, br] = *ritr;
-            if(f(a, b, ar, br) >= f(ar, br, arr, brr)){
-                slp_inc.erase(ritr);
-                rht_slp.erase(slp_rht[ar]);
-                slp_rht.erase(ar);
+            if(calc_(a, b, ar, br) >= calc_(ar, br, arr, brr)){
+                slope_to_intercept_.erase(ritr);
+                rightend_to_slope_.erase(slope_to_rightend_[ar]);
+                slope_to_rightend_.erase(ar);
                 ritr = next(itr);
             }
             else break;
         }
-        if(itr != slp_inc.begin()){
+        if(itr != slope_to_intercept_.begin()){
             auto [al, bl] = *prev(itr);
-            Point lrht = f(al, bl, a, b);
-            rht_slp.erase(slp_rht[al]);
-            slp_rht[al] = lrht;
-            rht_slp[lrht] = al;
+            Point lrht = calc_(al, bl, a, b);
+            rightend_to_slope_.erase(slope_to_rightend_[al]);
+            slope_to_rightend_[al] = lrht;
+            rightend_to_slope_[lrht] = al;
         }
-        if(next(itr) != slp_inc.end()){
+        if(next(itr) != slope_to_intercept_.end()){
             auto [ar, br] = *next(itr);
-            Point rht = f(a, b, ar, br);
-            if(slp_rht.contains(a)){
-                rht_slp.erase(slp_rht[a]);
+            Point rht = calc_(a, b, ar, br);
+            if(slope_to_rightend_.contains(a)){
+                rightend_to_slope_.erase(slope_to_rightend_[a]);
             }
-            slp_rht[a] = rht;
-            rht_slp[rht] = a;
+            slope_to_rightend_[a] = rht;
+            rightend_to_slope_[rht] = a;
         }
         else{
-            if(slp_rht.contains(a)){
-                rht_slp.erase(slp_rht[a]);
+            if(slope_to_rightend_.contains(a)){
+                rightend_to_slope_.erase(slope_to_rightend_[a]);
             }
-            slp_rht[a] = INF;
-            rht_slp[INF] = a;
+            slope_to_rightend_[a] = inf_;
+            rightend_to_slope_[inf_] = a;
         }
     }
 
     public:
-    ConvexHullTrick(bool isMax = false, bool AddMonotone = false, bool QueryMonotone = false) : 
-    is_max(isMax), add_monotone(AddMonotone), query_monotone(QueryMonotone){}
+    /**
+     * @brief Convex Hull Trick を初期化する。
+     * @param isMax 最大値クエリを処理する。
+     */
+    ConvexHullTrick(bool isMax = false) : is_max_(isMax){}
 
+    /**
+     * @brief 直線 `ax + b` を追加する。
+     */
     void insert(Line a, Line b){
-        if(is_max) a = -a, b = -b;
-        if(add_monotone);
-        else m_add(a, b);
+        if(is_max_) a = -a, b = -b;
+        else add_(a, b);
     }
 
+    /**
+     * @brief `x` における最小値(最大値)クエリを処理する。
+     */
     Line query(Point x){
-        auto [r, a] = *(rht_slp.lower_bound(x));
-        auto b = slp_inc[a];
-        if(is_max) a = -a, b = -b;
+        auto [r, a] = *(rightend_to_slope_.lower_bound(x));
+        auto b = slope_to_intercept_[a];
+        if(is_max_) a = -a, b = -b;
         return a * x + b;
     }
 };
---
+++ verify/LC-LineAddGetMin.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/line_add_get_min"
+
+#include "library/DataStructure/ConvexHullTrick.hpp"
+
+using ll = int64_t;
+
+int main(){
+    int N, Q; cin >> N >> Q;
+    ConvexHullTrick<ll> cht;
+    for(int i = 0; i < N; ++i){
+        ll a, b; cin >> a >> b;
+        cht.insert(a, b);
+    }
+    while(Q--){
+        int type; cin >> type;
+        if(type == 0){
+            ll a, b; cin >> a >> b;
+            cht.insert(a, b);
+        }
+        else{
+            ll p; cin >> p;
+            cout << cht.query(p) << endl;
+        }
+    }
+}
