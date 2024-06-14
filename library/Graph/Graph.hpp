1ebac7c54c85e991ee5232dc51140f1564118261
K-Yoshizwa <naniyoshi.z@gmail.com>
2024-06-14 16:53:27 +0900

    開発開始 #13

---
+++ library/Graph/Graph.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+ * @file Graph.hpp
+ * @brief Graph - グラフテンプレート
+ * @version 0.1
+ * @date 2024-06-14
+ */
+
+template<typename CostType>
+struct Edge{
+
+};
+
+template<typename CostType>
+class Graph{
+    
+};
