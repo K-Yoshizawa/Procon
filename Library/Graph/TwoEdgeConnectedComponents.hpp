616349bfc7b7f4221189e3feff95445dac1d04ae
log_K <36733349+K-Yoshizawa@users.noreply.github.com>
2024-10-23 00:13:56 +0900

    追加完了 [機能追加] 二辺連結成分分解の機能追加 #20

--- Library/Graph/TwoEdgeConnectedComponents.hpp
+++ Library/Graph/TwoEdgeConnectedComponents.hpp
@@ -6,6 +6,7 @@
  */
 
 #include "LowLink.hpp"
+#include "../Tree/Tree.hpp"
 
 template<typename CostType>
 class TwoEdgeConnectedComponents{
@@ -15,8 +16,10 @@ class TwoEdgeConnectedComponents{
      */
     TwoEdgeConnectedComponents(Graph<CostType> &graph) : G(graph){
         LowLink<CostType> lowlink(G);
+        belong_components_.resize(G.get_vertex_size(), -1);
+        vector<Edge<CostType>> bridges = lowlink.get_bridge();
         vector<bool> is_bridge(G.get_edge_size(), false);
-        for(Edge<CostType> &es : lowlink.get_bridge()){
+        for(Edge<CostType> &es : bridges){
             is_bridge[es.id] = true;
         }
         vector<bool> visited(G.get_vertex_size(), false);
@@ -25,10 +28,12 @@ class TwoEdgeConnectedComponents{
             if(visited[s]) continue;
             que.push(s);
             visited[s] = true;
+            int component_index = components_.size();
             components_.push_back({});
             while(que.size()){
                 Vertex v = que.front();
                 components_.back().push_back(v);
+                belong_components_[v] = component_index;
                 que.pop();
                 for(Edge<CostType> &es : G[v]){
                     if(is_bridge[es.id]) continue;
@@ -36,6 +41,15 @@ class TwoEdgeConnectedComponents{
                     que.push(es.to);
                     visited[es.to] = true;
                 }
+            }
+        }
+        adjacent_list_of_bridge_tree_.resize(components_.size());
+        for(int i = 0; i < bridges.size(); ++i){
+            if(is_bridge[bridges[i].id]){
+                Vertex u = get_belong_component(bridges[i].from);
+                Vertex v = get_belong_component(bridges[i].to);
+                adjacent_list_of_bridge_tree_[u].emplace_back(v, bridges[i].cost);
+                adjacent_list_of_bridge_tree_[v].emplace_back(u, bridges[i].cost);
             }
         }
     }
@@ -46,9 +60,37 @@ class TwoEdgeConnectedComponents{
     vector<vector<Vertex>> &get_components(){
         return components_;
     }
+
+    /**
+     * @brief 頂点 `v` が所属する二辺連結成分のインデックスを返す。
+     */
+    int get_belong_component(Vertex v) const {
+        return belong_components_[v];
+    }
+
+    /**
+     * @brief 二辺連結成分を縮約した根付き木を生成する。
+     * @note verify : https://atcoder.jp/contests/arc039/tasks/arc039_d
+     * @return RootedTree<CostType> 二辺連結成分を縮約した根付き木
+     */
+    RootedTree<CostType> ConstructBridgeTree() const {
+        int V = (int)components_.size();
+        RootedTree<CostType> ret(V);
+        auto dfs = [&](auto self, Vertex v, Vertex p) -> void {
+            for(auto [u, c] : adjacent_list_of_bridge_tree_[v]){
+                if(u == p) continue;
+                ret.AddEdge(v, u, c);
+                self(self, u, v);
+            }
+        };
+        dfs(dfs, 0, -1);
+        return ret;
+    }
     
     private:
     Graph<CostType> &G;
 
     vector<vector<Vertex>> components_;
+    vector<int> belong_components_;
+    vector<vector<pair<int, CostType>>> adjacent_list_of_bridge_tree_;
 };
