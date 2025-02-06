240eeb9d20e12a57535430313694f2704a27ab6f
lX57 <36733349+K-Yoshizawa@users.noreply.github.com>
2025-02-07 04:08:13 +0900

    Rollback Union-Find Union-Find #32

---
+++ Library/DataStructure/RollbackUnionFind.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+/**
+ * @file RollbackUnionFind.hpp
+ * @author lX57
+ * @brief Rollback Union-Find - Rollback 可能 Union-Find
+ */
+
+#include "../Common.hpp"
+
+class RollbackUnionFind{
+    public:
+    /**
+     * @brief 要素数 `size` で Union-Find を初期化します。
+     * @note 要素番号は 0-index です。
+     * @param size 要素数
+     */
+    RollbackUnionFind(size_t size) : data_(size, -1){}
+
+    /**
+     * @brief 要素 `k` が属する集合の代表元を返します。
+     * @param k 探索する要素
+     * @return 要素 `k` が属する集合の代表元
+     */
+    int Find(const int k) const {
+        if(data_[k] < 0) return k;
+        return Find(data_[k]);
+    }
+
+    /**
+     * @brief 要素 `x` と要素 `y` が同じ集合に属しているかを判定します。
+     * @param x 要素番号
+     * @param y 要素番号
+     * @return 同じ集合に属しているか
+     */
+    bool Same(const int x, const int y) const {
+        return Find(x) == Find(y);
+    }
+
+    /**
+     * @brief 要素 `x` と要素 `y` を併合します。併合した場合は `true` を返します。
+     * @param x 要素番号
+     * @param y 要素番号
+     * @return 併合を行ったか
+     */
+    bool Unite(int x, int y){
+        x = Find(x), y = Find(y);
+        history_.push({x, data_[x]});
+        history_.push({y, data_[y]});
+        if(x == y) return false;
+        if(data_[x] > data_[y]) swap(x, y);
+        data_[x] += data_[y];
+        data_[y] = x;
+        return true;
+    }
+
+    /**
+     * @brief 直前の `Unite` 操作を取り消します。
+     */
+    void Undo(){
+        auto [y, dy] = history_.top();
+        history_.pop();
+        data_[y] = dy;
+        auto [x, dx] = history_.top();
+        history_.pop();
+        data_[x] = dx;
+    }
+
+    /**
+     * @brief 各集合に所属する要素を列挙します。
+     * @return vector<vector<int>> 各集合に所属する要素の一覧
+     */
+    vector<vector<int>> Group() const {
+        vector<vector<int>> ret(data_.size());
+        for(int i = 0; i < data_.size(); ++i){
+            ret[Find(i)].emplace_back(i);
+        }
+        ret.erase(remove_if(begin(ret), end(ret), [&](vector<int> &v){
+            return v.empty();
+        }), end(ret));
+        return ret;
+    }
+
+    /**
+     * @brief 要素 `k` が属する集合の要素数を求めます。
+     * @param k 要素番号
+     * @return int 集合の要素数
+     */
+    int Size(const int k) const {
+        return -data_[Find(k)];
+    }
+
+    private:
+    vector<int> data_;
+    stack<pair<int, int>> history_;
+};
---
+++ verify/LC-PersistentUnionfind.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/persistent_unionfind"
+
+#include "../Library/DataStructure/RollbackUnionFind.hpp"
+
+int main(){
+    int N, Q; cin >> N >> Q;
+    using qs = tuple<int, int, int, int>;
+    vector<vector<qs>> G(Q + 1);
+    for(int i = 1; i <= Q; ++i){
+        int t, k, u, v; cin >> t >> k >> u >> v, ++k;
+        G[k].push_back({t, i, u, v});
+    }
+
+    RollbackUnionFind uf(N);
+    vector<int> ans(Q + 1, -1);
+    auto dfs = [&](auto self, const qs &query) -> void {
+        auto [t, i, u, v] = query;
+        if(t == 1){
+            ans[i] = uf.Same(u, v);
+        }
+        else{
+            if(t == 0) uf.Unite(u, v);
+            for(auto nquery : G[i]) self(self, nquery);
+            if(t == 0) uf.Undo();
+        }
+    };
+    dfs(dfs, {-1, 0, 0, 0});
+    for(auto a : ans){
+        if(a != -1) cout << a << endl;
+    }
+}
