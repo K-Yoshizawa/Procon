d388f2029a8e407e83af04555ef22a6584a465e2
lX57 <36733349+K-Yoshizawa@users.noreply.github.com>
2026-02-10 04:05:12 +0900

    遅延セグ木にも噓が書いてあった

---
+++ verify/AOJ-DSL-2-G.test.cpp
@@ -0,0 +1,46 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_G"
+
+#include "../Library/Template.hpp"
+#include "../Library/DataStructure/LazySegmentTree.hpp"
+
+using OperatorMonoid = int;
+
+struct Monoid{
+    long long val, len;
+
+    Monoid(long long v = 0) : val(v), len(1){}
+    Monoid(long long v, long long l) : val(v), len(l){}
+
+    static Monoid Merge(const Monoid &l, const Monoid &r){
+        return Monoid(l.val + r.val, l.len + r.len);
+    }
+
+    static Monoid Mapping(const Monoid &m, const OperatorMonoid o){
+        return Monoid(m.val + o * m.len, m.len);
+    }
+};
+
+int main(){
+    cin.tie(0)->sync_with_stdio(false);
+    int n, q; cin >> n >> q;
+
+    LazySegmentTree<Monoid, OperatorMonoid> seg(
+        n,
+        [&](Monoid l, Monoid r){return Monoid::Merge(l, r);},
+        [&](Monoid m, OperatorMonoid o){return Monoid::Mapping(m, o);},
+        [&](OperatorMonoid l, OperatorMonoid r){return l + r;},
+        Monoid(),
+        0
+    );
+    seg.Build();
+    while(q--){
+        int com, s, t, x; cin >> com >> s >> t;
+        if(com == 0){
+            cin >> x;
+            seg.Update(s, t + 1, x);
+        }
+        else{
+            cout << seg.Product(s, t + 1).val << '\n';
+        }
+    }
+}
