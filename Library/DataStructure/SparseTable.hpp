daa4bb070ebf051ff2c0ec1393a077700b9694e7
lX57 <36733349+K-Yoshizawa@users.noreply.github.com>
2026-06-01 13:29:25 +0900

    Sparse Table の更新

--- Library/DataStructure/SparseTable.hpp
+++ Library/DataStructure/SparseTable.hpp
@@ -2,36 +2,28 @@
 
 #include "../Common.hpp"
 
-template<typename IMonoid>
+template<typename ValueType>
 class SparseTable{
     public:
-    SparseTable() = default;
-
-    SparseTable(
-        const vector<IMonoid> &A,
-        const function<IMonoid(IMonoid, IMonoid)> f = [](IMonoid x, IMonoid y){
-            return min(x, y);
-        }
-    ) : op_(f){
-        N_ = (int)A.size(), rows_ = 0;
-        while(1 << (rows_ + 1) <= N_) ++rows_;
-        data_.resize(rows_ + 1, vector<IMonoid>(N_ + 1));
-        for(int i = 0; i < N_; ++i) data_[0][i] = A[i];
-        for(int k = 0; k < rows_; ++k){
-            for(int i = 0; i + (1 << k) <= N_; ++i){
-                data_[k + 1][i] = op_(data_[k][i], data_[k][i + (1 << k)]);
+    SparseTable(const vector<ValueType> &A) : N((int)A.size()){
+        int row = 0;
+        while(1 << (row + 1) <= N) ++row;
+        data_.resize(row + 1, vector<ValueType>(N + 1));
+        for(int i = 0; i < N; ++i) data_[0][i] = A[i];
+        for(int k = 0; k < row; ++k){
+            for(int i = 0; i + (1 << k) <= N; ++i){
+                data_[k + 1][i] = min(data_[k][i], data_[k][i + (1 << k)]);
             }
         }
     }
 
-    inline IMonoid Query(const int &l, const int &r) const {
-        assert(0 <= l && l < r && r <= N_);
+    inline ValueType Fold(int l, int r) const {
+        assert(0 <= l && l < r && r <= N);
         int k = bit_width((uint32_t)r - l) - 1;
-        return op_(data_[k][l], data_[k][r - (1 << k)]);
+        return min(data_[k][l], data_[k][r - (1 << k)]);
     }
 
     private:
-    vector<vector<IMonoid>> data_;
-    function<IMonoid(IMonoid, IMonoid)> op_;
-    int N_, rows_;
+    vector<vector<ValueType>> data_;
+    int N;
 };
